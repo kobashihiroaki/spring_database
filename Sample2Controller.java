bfce0b21aba2f97762b7ee2ca2f327479d16fcb7
kobashihiroaki <h.kobashi@g-waters.com>
2020-09-28 19:07:54 +0900

    practice

--- Sample2Controller.java
+++ Sample2Controller.java
@@ -1,14 +1,22 @@
 package com.tuyano.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tuyano.springboot.repositories.MyDataRepository;
+
 @Controller
 public class Sample2Controller {
+	@Autowired
+	MyDataRepository repository;
 	@RequestMapping("/")
 	public ModelAndView index(ModelAndView mav) {
 		mav.setViewName("sample2");
+		mav.addObject("msg", "this is sample content");
+		Iterable<MyData> list = repository.findAll();
+		mav.addObject("data", list);
 		return mav;
 	}
 }
---
+++ repositories/MyDataRepository.java
@@ -0,0 +1,12 @@
+package com.tuyano.springboot.repositories;
+
+import com.tuyano.springboot.MyData;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyDataRepository extends JpaRepository<MyData, Long> {
+	
+}
