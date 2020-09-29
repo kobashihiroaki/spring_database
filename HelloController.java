dd0288cd57520d4f4397730b432575e7d1a39f19
kobashihiroaki <h.kobashi@g-waters.com>
2020-09-29 17:58:58 +0900

    バリデーション処理

--- HelloController.java
+++ HelloController.java
@@ -1,11 +1,19 @@
 package com.tuyano.springboot;
 
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.tuyano.springboot.repositories.MyDataRepository;
@@ -20,6 +28,7 @@ public class HelloController {
 		    ModelAndView mav) {
 		mav.setViewName("index");
 		mav.addObject("msg", "this is sample content.");
+		mav.addObject("formModel", mydata);
 		Iterable<MyData> list = repository.findAll();
 		mav.addObject("datalist", list);
 		return mav;
@@ -28,9 +37,82 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	@Transactional(readOnly=false)
 	public ModelAndView form(
-			@ModelAttribute("formModel") MyData mydata,
+			@ModelAttribute("formModel")
+			@Validated MyData mydata,
+			BindingResult result,
+			ModelAndView mav) {
+		ModelAndView res = null;
+		if (!result.hasErrors()) {
+			repository.saveAndFlush(mydata);
+			res = new ModelAndView("redirect:/");
+		} else {
+			mav.setViewName("index");
+			mav.addObject("msg", "sorry, error is occured...");
+			Iterable<MyData> list = repository.findAll();
+			mav.addObject("datalist", list);
+			res = mav;
+		}
+		return res;
+	}
+	
+	@PostConstruct
+	public void init() {
+		//1つ目のダミーデータ作成
+		MyData d1 = new MyData();
+		d1.setName("tuyano");
+		d1.setAge(123);
+		d1.setMail("syoda@tuyano.com");
+		d1.setMemo("this is my data!");
+		repository.saveAndFlush(d1);
+		//2つ目のダミーデータ作成
+		MyData d2 = new MyData();
+		d2.setName("hanako");
+		d2.setAge(15);
+		d2.setMail("hanako@flower");
+		d2.setMemo("my girl friend.");
+		repository.saveAndFlush(d2);
+		//3つ目のダミーデータ作成
+		MyData d3 = new MyData();
+		d3.setName("sachiko");
+		d3.setAge(37);
+		d3.setMail("sachiko@happy");
+		d3.setMemo("my work friend");
+		repository.saveAndFlush(d3);
+	}
+	
+	@RequestMapping(value= "/edit/{id}", method = RequestMethod.GET)
+	public ModelAndView edit(@ModelAttribute MyData mydata,
+			@PathVariable int id, ModelAndView mav) {
+		mav.setViewName("edit");
+		mav.addObject("title", "edit mydata.");
+		Optional<MyData> data = repository.findById((long) id);
+		mav.addObject("formModel", data.get());
+		return mav;
+	}
+	
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView update(@ModelAttribute MyData mydata,
 			ModelAndView mav) {
 		repository.saveAndFlush(mydata);
+		return new ModelAndView("redirect:/");
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public ModelAndView delete(@PathVariable int id,
+			ModelAndView mav) {
+		mav.setViewName("delete");
+		mav.addObject("title", "delete mydata");
+		Optional<MyData> data = repository.findById((long)id);
+		mav.addObject("formModel", data.get());
+		return mav;
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.POST)
+	@Transactional(readOnly=false)
+	public ModelAndView remove(@RequestParam long id,
+			ModelAndView mav) {
+		repository.deleteById(id);
 		return new ModelAndView("redirect:/");
 	}
 }
--- MyData.java
+++ MyData.java
@@ -7,22 +7,36 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+/*
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+*/
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 @Entity
 @Table(name="mydata")
 public class MyData {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column
+	@NotNull
 	private long id;
 	
+	@NotEmpty
 	@Column(length = 50, nullable = false)
 	private String name;
 	
 	@Column(length = 200, nullable = true)
+	@Email
 	private String mail;
 	
 	@Column(nullable = true)
+	@Min(0)
+	@Max(200)
 	private Integer age;
 	
 	@Column(nullable = true)
